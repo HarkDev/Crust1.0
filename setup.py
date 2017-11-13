e2f0849b1c377346088d8ca3b5fd6e78e0d2ccc4
HarkDev <juan@harkdev.com>
2017-11-13 16:23:40 -0300

    - Renamed package to crust1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-setup(name='pycrust10',
+setup(name='crust1',
       version='1.0.1',
       description='A python package to retreive information from the LLNL Crust 1.0 model.',
       author='John Leeman, Juan Manuel Haedo',
       author_email='john@leemangeophysical.com, juanu@juanu.com.ar',
       url='https://github.com/jrleeman/Crust1.0/',
-      packages=['pycrust10'], # Package name
-      package_data={'pycrust10': ['data/*.*']}, # Include data files
+      packages=['crust1'], # Package name
+      package_data={'crust1': ['data/*.*']}, # Include data files
       include_package_data=True
      )
